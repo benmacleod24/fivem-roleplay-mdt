930dcf89a03807a3c0bd51064870ba2a4b639560
Ben Macleod <benstreasurebox@gmail.com>
2021-09-25 15:25:07 -0400

    dev data button

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,21 +1,31 @@
-import { Box, Flex, Heading } from '@chakra-ui/react';
+import { Box, Button, Flex, Heading } from '@chakra-ui/react';
 import { useSession } from 'next-auth/client';
 import * as React from 'react';
 import Layout from '../../components/layout';
 
-export interface ProfileProps {
-
-}
+export interface ProfileProps {}
 
 const Profile: React.FunctionComponent<ProfileProps> = ({}) => {
   const [session, loading] = useSession();
 
+  const onDevDatRequest = () => {
+    navigator.clipboard.writeText(`Dev Data:
+    isCop: ${session?.user.isCop}
+    isJudge: ${session?.user.isJudge}
+    copId: ${session?.user.copId}
+    copName: ${session?.user.copName}
+    discordID: ${session?.user.id}`);
+  };
+
   return (
     <Layout>
-      <Flex>
+      <Flex flexDir="column">
         <Box background="gray.700" borderRadius="md" width="100%" p="3">
-          <Heading size="lg">My Profile</Heading>
+          <Heading size="md">My Profile</Heading>
         </Box>
+        <Button mt="3" w="fit-content" colorScheme="yellow" onClick={onDevDatRequest}>
+          Copy Dev Data
+        </Button>
       </Flex>
     </Layout>
   );
